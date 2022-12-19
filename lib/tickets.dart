9222da0cbfcceaa6c5bb2e99024bb441040bd043
Prathik <prathik612@gmail.com>
2022-12-19 17:42:31 +0300

    custom amount (#2)

--- lib/tickets.dart
+++ lib/tickets.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Tickets extends StatelessWidget {
+class Tickets extends StatefulWidget {
   const Tickets({super.key});
 
+  @override
+  State<Tickets> createState() => _TicketsState();
+}
+
+class _TicketsState extends State<Tickets> {
+  var balance = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Tickets'),
+        child: Text('Tickets')
       ),
     );
   }
